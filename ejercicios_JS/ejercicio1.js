72f07cdf01ccaf9ac516ba634b17507347bd0e6f
Sara Oliva <s4r40l1v4@gmail.com>
2026-07-29 17:04:08 +0200

    Add ejercicio1.js with favorite hero and calculations

---
+++ ejercicios_JS/ejercicio1.js
@@ -0,0 +1,14 @@
+// Ejercicio 1
+
+let miSuperheroeFavorito = "Hulk";
+
+let numeroFavorito = 50;
+
+let altura = 5;
+
+let longitud = 10;
+
+let suma = altura + longitud;
+
+console.log("Mi superhéroe favorito es: ", miSuperheroeFavorito, ". Mi número favorito es: ", numeroFavorito);
+console.log("La altura es: ", altura, " y la longitud: ", longitud, ", así que la suma es: ", suma);
