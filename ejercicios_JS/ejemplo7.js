45a56641a639995826086ed2977fe3c81efbdf05
Sara Oliva <s4r40l1v4@gmail.com>
2026-07-29 17:55:51 +0200

    Add function to determine the greater of two numbers

---
+++ ejercicios_JS/ejemplo7.js
@@ -0,0 +1,10 @@
+function greaterNumber(numberOne, numberTwo) {
+  if (numberOne > numberTwo) {
+    console.log(numberOne);
+  } else {
+    console.log(numberTwo);
+  }
+}
+
+// Ejemplo
+greaterNumber(10, 5); 
