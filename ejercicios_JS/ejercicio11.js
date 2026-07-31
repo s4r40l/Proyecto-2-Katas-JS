db6eaa19c6cb03ca2246c49f333bd1f3586364e9
Sara Oliva <s4r40l1v4@gmail.com>
2026-07-31 10:07:05 +0200

    Implement average calculation for mixed elements
    Added a function to calculate the average of numbers and string lengths in an array.

---
+++ ejercicios_JS/ejercicio11.js
@@ -0,0 +1,26 @@
+const mixedElements = [
+  6,
+  1,
+  "Marvel",
+  1,
+  "hamburguesa",
+  "10",
+  "Prometeo",
+  8,
+  "Hola mundo",
+];
+function averageWord(list) {
+  let total = 0;
+
+  for (let i = 0; i < list.length; i++) {
+    if (typeof list[i] === "number") {
+      total += list[i];
+    } else {
+      total += list[i].length;
+    }
+  }
+
+  console.log(total);
+}
+
+averageWord(mixedElements);
