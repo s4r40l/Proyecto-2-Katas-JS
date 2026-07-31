efec6e463f53e0b3b8642a75401ed02869514c6a
Sara Oliva <s4r40l1v4@gmail.com>
2026-07-31 10:25:26 +0200

    Add function to display travel destinations

---
+++ ejercicios_JS/ejercicio_16.js
@@ -0,0 +1,16 @@
+const placesToTravel = [
+  "Japon",
+  "Venecia",
+  "Murcia",
+  "Santander",
+  "Filipinas",
+  "Madagascar",
+];
+
+function showPlaces(placesToTravel) {
+  for (const place of placesToTravel) {
+    console.log(place);
+  }
+}
+
+showPlaces(placesToTravel);
