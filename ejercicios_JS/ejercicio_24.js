c14bc469628241d51807fcf277214c596e0084b6
Sara Oliva <s4r40l1v4@gmail.com>
2026-07-31 10:55:40 +0200

    Add function to filter and log human characters

---
+++ ejercicios_JS/ejercicio_24.js
@@ -0,0 +1,26 @@
+const characters = [
+  { name: 'Luke Skywalker', species: 'Human' },
+  { name: 'Darth Vader', species: 'Human' },
+  { name: 'Chewbacca', species: 'Wookiee' },
+  { name: 'Leia Organa', species: 'Human' },
+  { name: 'R2-D2', species: 'Droid' },
+  { name: 'C-3PO', species: 'Droid' },
+  { name: 'Obi-Wan Kenobi', species: 'Human' },
+  { name: 'Yoda', species: 'Unknown' },
+  { name: 'Han Solo', species: 'Human' }
+];
+
+const humanCharacters = [];
+
+function getHumans(characters) {
+
+  for (const character of characters) {
+    if (character.species === "Human") {
+      humanCharacters.push(character.name);
+    }
+  }
+
+  console.log("Personajes humanos:", humanCharacters);
+}
+
+getHumans(characters);
