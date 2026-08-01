027f20f4552b130665e5be787478f7b6cb0af60a
Sara Oliva <s4r40l1v4@gmail.com>
2026-08-01 15:54:03 +0200

    Implement average movie duration calculation
    Added a function to calculate the average duration of movies.

---
+++ ejercicios_JS/ejercicio_34.js
@@ -0,0 +1,24 @@
+const movies = [
+  { title: 'Inception', duration: 148 },
+  { title: 'The Dark Knight', duration: 152 },
+  { title: 'Interstellar', duration: 169 },
+  { title: 'Dunkirk', duration: 106 },
+  { title: 'The Prestige', duration: 130 },
+  { title: 'Memento', duration: 113 },
+  { title: 'Batman Begins', duration: 140 },
+  { title: 'The Dark Knight Rises', duration: 164 },
+  { title: 'Tenet', duration: 150 },
+  { title: 'Insomnia', duration: 118 }
+];
+
+function averageMovieDuration(movies) {
+  let total = 0;
+
+  for (const movie of movies) {
+    total += movie.duration;
+  }
+
+  return total / movies.length;
+}
+
+console.log(averageMovieDuration(movies));
