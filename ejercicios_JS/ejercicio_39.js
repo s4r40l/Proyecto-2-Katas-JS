b304b2059da9ae4f167834e5c1dfd04a2e35cbac
Sara Oliva <s4r40l1v4@gmail.com>
2026-08-01 16:19:09 +0200

    Create ejercicio_39.js to count favorite sounds
    Added a script to count favorite sounds from users.

---
+++ ejercicios_JS/ejercicio_39.js
@@ -0,0 +1,50 @@
+const users = [
+  {
+    name: "Alberto",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 50 },
+      rain: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Antonio",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 30 },
+      shower: { format: "ogg", volume: 55 },
+      train: { format: "mp3", volume: 60 },
+    },
+  },
+  {
+    name: "Santiago",
+    favoritesSounds: {
+      shower: { format: "mp3", volume: 50 },
+      train: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Laura",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 67 },
+      wind: { format: "ogg", volume: 35 },
+      firecamp: { format: "mp3", volume: 60 },
+    },
+  },
+];
+
+const soundCount = {};
+
+for (const user of users) {
+  for (const sound in user.favoritesSounds) {
+
+    if (!soundCount[sound]) {
+      soundCount[sound] = 1;
+    } else {
+      soundCount[sound]++;
+    }
+
+  }
+}
+
+console.log(soundCount);
